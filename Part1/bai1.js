43926dc23e526472d6409e5982c42803c6ed1cab
tranvy57 <tranvy.art@gmail.com>
2024-08-19 15:51:51 +0700

    add bai1

---
+++ Part1/bai1.js
@@ -0,0 +1,10 @@
+var markMass = 78;
+var markHeight = 1.69;
+var johnMass = 92;
+var johnHeight = 1.95;
+var markBMI = markMass/(markHeight*markHeight);
+var johnBMI = johnMass/(johnHeight*johnHeight)
+var markHigherBMI = markBMI>johnBMI
+console.log(markHigherBMI);
+
+
