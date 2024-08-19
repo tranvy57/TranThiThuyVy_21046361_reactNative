c0964face63a455d034c236b44ca728464934e07
tranvy57 <tranvy.art@gmail.com>
2024-08-19 17:10:13 +0700

    add part2_bai2

---
+++ Buoi1/part2_bai2.js
@@ -0,0 +1,14 @@
+const calcTip = (bill) => {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+console.log(calcTip(100));
+
+bills = [125, 555, 44];
+tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills);
+console.log(tips);
+
+totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(totals);
+
