0e945d0c51d0fd916d7cec25952df08623a36a97
tranvy57 <tranvy.art@gmail.com>
2024-09-07 21:30:14 +0700

    add oop#2

---
+++ Buoi2/OOp/bai2.js
@@ -0,0 +1,42 @@
+// 1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
+// 2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
+// by 1.6)
+// 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
+// converts it to km/h before storing the value, by multiplying the input by 1.6)
+// 4. Create a new car and experiment with the 'accelerate' and 'brake'
+// methods, and with the getter and setter.
+// Test data:
+// § Data car 1: 'Ford' going at 120 km/h
+
+class CarCl {
+	constructor(make, speed) {
+		this.make = make
+		this.speed = speed
+	}
+
+	accelerate() {
+		this.speed += 10
+		console.log(this.speed)
+	}
+
+	brake() {
+		this.speed -= 5
+		console.log(this.speed)
+	}
+
+	get speedUS() {
+		return this.speed / 1.6
+	}
+
+	set speedUS(speed) {
+		this.speed = speed * 1.6
+	}
+}
+
+const car1 = new CarCl('Ford', 120)
+console.log(car1.speedUS)
+car1.accelerate()
+car1.accelerate()
+car1.brake()
+console.log(car1.speedUS)
+car1.speedUS = 50
